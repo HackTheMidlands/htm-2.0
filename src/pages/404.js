df591d68f240528200110dbe4161b6a121219e48
Alex Wiley <alex.wiley@oneupsales.co.uk>
2020-10-19 21:41:48 +0100

    Added 404

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Layout from '../components/layout/layout';
+import { HeroHeader } from '../components/hero-header/hero-header';
+
+export default () => (
+    <Layout>
+        <HeroHeader>
+            <h1>Page Not Found</h1>
+        </HeroHeader>
+    </Layout>
+);
