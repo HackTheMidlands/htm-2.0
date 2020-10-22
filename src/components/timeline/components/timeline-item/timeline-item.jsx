cef6aa17a2a1cc8dcfdf3579ec8d782f219c0797
Charles de Freitas <me@chasbob.dev>
2020-10-22 16:44:36 +0100

    Add end times

--- src/components/timeline/components/timeline-item/timeline-item.jsx
+++ src/components/timeline/components/timeline-item/timeline-item.jsx
@@ -16,17 +16,18 @@ import style from './timeline-item.module.scss';
  * Timeline item box
  * @param name
  * @param time
+ * @param endTime
  * @param owner
  * @param state
  * @returns {*}
  * @constructor
  */
 export const TimelineItem = ({
-    name, time, owner, state,
+    name, time, endTime, owner, state,
 }) => (
     <div className={classNames([style.timelineItem, style[state]])}>
         <h3 className={style.name}>{ name }</h3>
-        <p className={style.time}>{ time }</p>
+        <p className={style.time}>{ time } - { endTime }</p>
         <p className={style.owner}>{ owner }</p>
     </div>
 );
@@ -35,6 +36,7 @@ export const TimelineItem = ({
 TimelineItem.propTypes = {
     name: PropTypes.string.isRequired,
     time: PropTypes.string.isRequired,
+    endTime: PropTypes.string.isRequired,
     owner: PropTypes.string.isRequired,
     state: PropTypes.oneOf(['active', 'inactive']),
 };
