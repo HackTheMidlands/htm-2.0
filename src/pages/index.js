06b6d8569669b1bae0f28c71bde86d20c5835c31
Charles de Freitas <me@chasbob.dev>
2021-07-10 18:34:50 +0100

    Add Q&A from contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -133,25 +133,6 @@ const partners = [];
 
 const tickets = [];
 
-const qa = [
-    {
-        question: 'Can Under 18s Attend?',
-        answer: 'YES! Anyone is welcome at the event, and we’ll be ensuring that the Discord is a safe and friendly environment for everyone 🥰',
-    },
-    {
-        question: 'What do I need?',
-        answer: 'A computer 💻, a Discord account 👾, and a passion to get involved 🔥 We’d recommend a headset to communicate with your team, but that’s not essential! 🎧',
-    },
-    {
-        question: "What if I don't know how to code?\n",
-        answer: "That is not a problem! A great way to learn how to code is by attending a hackathon 👩‍💻 It's an event for like-minded people to share what they are interested in and teach each-other what they are passionate about, whether it's coding, electronics or anything else you want to know about 💡 You never know what new skill you might pick up! You can also ask specific questions to our mentors and other participants in the Discord server 🙋 We’re all here to help!\n",
-    },
-    {
-        question: "I don't have a team\n",
-        answer: "Don't worry! Many people don't have a team when they turn up to a hackathon. We have a dedicated channel on our Discord server (#looking-for-team) where people are looking for teams, and teams are looking for members, and we’ll be running an icebreaker session 🧊 to help you find teams on the day👋",
-    },
-];
-
 /**
  * Hackathon template
  * @param props
@@ -173,8 +154,18 @@ const Index = (props) => {
                     secure_url
                 }
             }
+            allContentfulQa {
+                nodes {
+                    question
+                    answer {
+                        answer
+                    }
+                }
+            }
         }
     `);
+    const qa = data.allContentfulQa.nodes.map(({question, answer}) => ({ question, 'answer': answer.answer}));
+            console.log(qa);
     const { title, video } = data.contentfulPage;
     const sources = [
         {
