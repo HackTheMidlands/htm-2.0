77b3632793e4a02346caa93b37d67dcfe46ffce0
Charles de Freitas <me@chasbob.dev>
2021-07-10 17:39:49 +0100

    Initial addition of contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,39 @@
-import React, { useEffect } from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-import moment from 'moment';
-import { Link, graphql, useStaticQuery } from 'gatsby';
-import withReactContent from 'sweetalert2-react-content';
-
-// Style import
-import style from './index.module.scss';
-
+import PartyFace from '../assets/gifs/partying_face.gif';
+import HTMVideo from '../assets/hero-video.mp4';
+import Discord from '../assets/img/discord-white.svg';
+import { ButtonWithIcon } from '../components/button-with-icon/button-with-icon';
+import { Button } from '../components/button/button';
+import { EventInfo } from '../components/event-info/event-info';
+import { EventIntroStatistics } from '../components/event-intro-statistics/event-intro-statistics';
+import { EventLocation } from '../components/event-location/event-location';
+import { FaqSection } from '../components/faq-section/faq-section';
+import { HeroHeader } from '../components/hero-header/hero-header';
+import { IconCard } from '../components/icon-card/icon-card';
+import { IndexIntro } from '../components/index-intro/index-intro';
 // Components
 import Layout from '../components/layout/layout.js';
-import { HeroHeader } from '../components/hero-header/hero-header';
-import { Button } from '../components/button/button';
-import { Video } from '../components/video/video';
-import { EventIntroStatistics } from '../components/event-intro-statistics/event-intro-statistics';
-import { EventInfo } from '../components/event-info/event-info';
-import { IconCard } from '../components/icon-card/icon-card';
-import { EventLocation } from '../components/event-location/event-location';
-import { TestimonialSliderSection } from '../components/testimonial-slider-section/testimonial-slider-section';
-import { SponsorsSection } from '../components/sponsors-section/sponsors-section';
 import { SponsorCta } from '../components/sponsor-cta/sponsor-cta';
-import { TicketsSection } from '../components/tickets-section/tickets-section';
-import { FaqSection } from '../components/faq-section/faq-section';
+import { SponsorsSection } from '../components/sponsors-section/sponsors-section';
 import { StayConnectedSection } from '../components/stay-connected-section/stay-connected-section';
-import { IndexIntro } from '../components/index-intro/index-intro';
-
+import { TestimonialSliderSection } from '../components/testimonial-slider-section/testimonial-slider-section';
+import { TicketsSection } from '../components/tickets-section/tickets-section';
+import DemoVideo from '../components/video/assets/example-video.mp4';
+import { Video } from '../components/video/video';
+import CalendarIcon from './assets/calendar.inline.svg';
+import DiscordSquareIcon from './assets/discord-square.svg';
+import EntryRequirementsIcon from './assets/entry.svg';
+import FoodAndDrinkIcon from './assets/food-and-drink.svg';
 // Image imports
 import LocationInfo from './assets/location.inline.svg';
-import CalendarIcon from './assets/calendar.inline.svg';
-import TimeIcon from './assets/time.svg';
-import DiscordSquareIcon from './assets/discord-square.svg';
-import FoodAndDrinkIcon from './assets/food-and-drink.svg';
 import SafeguardingIcon from './assets/safeguarding.svg';
-import EntryRequirementsIcon from './assets/entry.svg';
-import PartyFace from '../assets/gifs/partying_face.gif';
-
-import DemoVideo from '../components/video/assets/example-video.mp4';
-import HTMVideo from '../assets/hero-video.mp4';
-import { ButtonWithIcon } from '../components/button-with-icon/button-with-icon';
-import Discord from '../assets/img/discord-white.svg';
-
-const sources = [
-    {
-        src: HTMVideo,
-        type: 'video/mp4',
-    },
-];
+import TimeIcon from './assets/time.svg';
+// Style import
+import style from './index.module.scss';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import moment from 'moment';
+import React, { useEffect } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import withReactContent from 'sweetalert2-react-content';
 
 const poster = '/images/cover.jpg';
 
@@ -68,20 +57,19 @@ const EventDetails = ({ eventStart, eventEnd }) => {
         {
             icon: DiscordSquareIcon,
             title: 'Participate with Discord',
-            body:
-        'We’re running this years event through discord. Make sure to <a style="color: #7289DA" href="https://discord.gg/hackthemidlands" target="_blank">join the server</a> to get involved.',
+            body: 'We’re running this years event through discord. Make sure to <a style="color: #7289DA" href="https://discord.gg/hackthemidlands" target="_blank">join the server</a> to get involved.',
             colour: 'rgba(114, 137, 218, .5)',
         },
-    // {
-    //     icon: SafeguardingIcon,
-    //     title: 'Safeguarding',
-    //     body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.',
-    // },
-    // {
-    //     icon: EntryRequirementsIcon,
-    //     title: 'Entry Requirements',
-    //     body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.',
-    // },
+        // {
+        //     icon: SafeguardingIcon,
+        //     title: 'Safeguarding',
+        //     body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.',
+        // },
+        // {
+        //     icon: EntryRequirementsIcon,
+        //     title: 'Entry Requirements',
+        //     body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.',
+        // },
     ];
     return (
         <EventInfo>
@@ -89,15 +77,12 @@ const EventDetails = ({ eventStart, eventEnd }) => {
                 <Row>
                     <Col lg={10} lgOffset={1}>
                         <Row>
-                            {details.map(({
-                                icon, title, body, colour,
-                            }) => (
+                            {details.map(({ icon, title, body, colour }) => (
                                 <Col
                                     sm={12}
                                     md={6}
                                     className={style.eventDetailItem}
-                                    key={title}
-                                >
+                                    key={title}>
                                     <IconCard
                                         key={title}
                                         body={body}
@@ -158,12 +143,12 @@ const qa = [
         answer: 'A computer 💻, a Discord account 👾, and a passion to get involved 🔥 We’d recommend a headset to communicate with your team, but that’s not essential! 🎧',
     },
     {
-        question: 'What if I don\'t know how to code?\n',
-        answer: 'That is not a problem! A great way to learn how to code is by attending a hackathon 👩‍💻 It\'s an event for like-minded people to share what they are interested in and teach each-other what they are passionate about, whether it\'s coding, electronics or anything else you want to know about 💡 You never know what new skill you might pick up! You can also ask specific questions to our mentors and other participants in the Discord server 🙋 We’re all here to help!\n',
+        question: "What if I don't know how to code?\n",
+        answer: "That is not a problem! A great way to learn how to code is by attending a hackathon 👩‍💻 It's an event for like-minded people to share what they are interested in and teach each-other what they are passionate about, whether it's coding, electronics or anything else you want to know about 💡 You never know what new skill you might pick up! You can also ask specific questions to our mentors and other participants in the Discord server 🙋 We’re all here to help!\n",
     },
     {
-        question: 'I don\'t have a team\n',
-        answer: 'Don\'t worry! Many people don\'t have a team when they turn up to a hackathon. We have a dedicated channel on our Discord server (#looking-for-team) where people are looking for teams, and teams are looking for members, and we’ll be running an icebreaker session 🧊 to help you find teams on the day👋',
+        question: "I don't have a team\n",
+        answer: "Don't worry! Many people don't have a team when they turn up to a hackathon. We have a dedicated channel on our Discord server (#looking-for-team) where people are looking for teams, and teams are looking for members, and we’ll be running an icebreaker session 🧊 to help you find teams on the day👋",
     },
 ];
 
@@ -175,15 +160,29 @@ const qa = [
  */
 const Index = (props) => {
     const data = useStaticQuery(graphql`
-    query IndexEventTimeQuery {
-      site {
-        siteMetadata {
-          eventStart: eventStart
-          eventEnd: eventEnd
+        query IndexPageQuery {
+            site {
+                siteMetadata {
+                    eventStart: eventStart
+                    eventEnd: eventEnd
+                }
+            }
+            contentfulPage(ref: { eq: "index" }) {
+                title
+                video {
+                    secure_url
+                }
+            }
         }
-      }
-    }
-  `);
+    `);
+    const { title, video } = data.contentfulPage;
+    const sources = [
+        {
+            src: video[0].secure_url,
+            type: 'video/mp4',
+        },
+    ];
+
     const { eventStart, eventEnd } = data.site.siteMetadata;
     return (
         <Layout>
@@ -191,7 +190,7 @@ const Index = (props) => {
                 <Grid>
                     <Row>
                         <Col xs={12} sm={10} smOffset={1}>
-                            <h1 className={style.headerTitle}>HackTheMidlands 6.0</h1>
+                            <h1 className={style.headerTitle}>{title}</h1>
                             <ul className={style.eventInfoList}>
                                 <li className={style.eventInfoListItem}>
                                     <LocationInfo />
@@ -199,21 +198,35 @@ const Index = (props) => {
                                 </li>
                                 <li className={style.eventInfoListItem}>
                                     <CalendarIcon />
-                                    <p>{moment(eventStart).format('dddd Do')} - {moment(eventEnd).format('Do MMMM YYYY')}</p>
+                                    <p>
+                                        {moment(eventStart).format('dddd Do')} -{' '}
+                                        {moment(eventEnd).format(
+                                            'Do MMMM YYYY',
+                                        )}
+                                    </p>
                                 </li>
                             </ul>
                             <div className={style.headerButtons}>
                                 {/*<Link to="/live" target="_blank"> */}
-                                   {/* <Button theme="orange">View Live Page</Button> */}
-                               {/* </Link> */}
+                                {/* <Button theme="orange">View Live Page</Button> */}
+                                {/* </Link> */}
                                 {/* <a href="https://www.eventbrite.co.uk/e/hackthemidlands-50-tickets-111222359070" target="_blank"> */}
                                 {/*    <Button theme="orange">Get tickets</Button> */}
                                 {/* </a> */}
                                 {/* <a href="/sponsorship.pdf" target="_blank"> */}
                                 {/*    <Button theme="whiteSolid">Sponsor</Button> */}
                                 {/* </a> */}
-                                <a href="https://discord.gg/kx9azme" target="_blank" style={{ textDecoration: 'none' }}>
-                                    <ButtonWithIcon icon={Discord} name="Discord" backgroundColor="#6F87E0" border="2px solid #4f66bd">Join Discord</ButtonWithIcon>
+                                <a
+                                    href="https://discord.gg/kx9azme"
+                                    target="_blank"
+                                    style={{ textDecoration: 'none' }}>
+                                    <ButtonWithIcon
+                                        icon={Discord}
+                                        name="Discord"
+                                        backgroundColor="#6F87E0"
+                                        border="2px solid #4f66bd">
+                                        Join Discord
+                                    </ButtonWithIcon>
                                 </a>
                             </div>
                         </Col>
@@ -225,7 +238,11 @@ const Index = (props) => {
                 <Grid>
                     <Row>
                         <Col lg={8} lgOffset={2}>
-                            <Video sources={sources} poster={poster} controls={false} />
+                            <Video
+                                sources={sources}
+                                poster={poster}
+                                controls={false}
+                            />
                         </Col>
                     </Row>
                 </Grid>
@@ -244,7 +261,13 @@ const Index = (props) => {
 
             {/* <TestimonialSliderSection testimonials={testimonials} /> */}
 
-            <SponsorsSection goldTier={gold} silverTier={silver} bronzeTier={bronze} digitalTier={digital} partners={partners} />
+            <SponsorsSection
+                goldTier={gold}
+                silverTier={silver}
+                bronzeTier={bronze}
+                digitalTier={digital}
+                partners={partners}
+            />
 
             <SponsorCta />
 
@@ -253,7 +276,6 @@ const Index = (props) => {
             <FaqSection questions={qa} />
 
             <StayConnectedSection />
-
         </Layout>
     );
 };
--- src/pages/team.js
+++ src/pages/team.js
@@ -1,141 +1,73 @@
+import { HeroHeader } from '../components/hero-header/hero-header';
+// Components
+import Layout from '../components/layout/layout';
+import { TeamCard } from '../components/team-card/team-card';
+// Assets
+import FireGif from './assets/fire.gif';
+// Style
+import style from './team.module.scss';
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
-// Components
-import Layout from '../components/layout/layout';
-import { HeroHeader } from '../components/hero-header/hero-header';
-import { TeamCard } from '../components/team-card/team-card';
+const Team = () => {
+    const data = useStaticQuery(graphql`
+        query TeamPageQuery {
+            allContentfulTeamMember {
+                nodes {
+                    nickname
+                    name
+                    image {
+                        gatsbyImageData
+                    }
+                    position
+                }
+            }
+        }
+    `);
 
-// Style
-import style from './team.module.scss';
+    const teamMembers = data.allContentfulTeamMember.nodes;
 
-// Assets
-import FireGif from './assets/fire.gif';
+    return (
+        <Layout>
+            <HeroHeader style={{ paddingBottom: '20vw' }}>
+                <h1 className={style.title}>Meet The Team</h1>
+            </HeroHeader>
 
-// Team Pictures
-import AlexW from './assets/htm-people/alex.jpg';
-import Charlie from './assets/htm-people/CharliedeFreitas.jpg';
-import DanielS from './assets/htm-people/DanielSpencer.jpg';
-import Fraser from './assets/htm-people/FraserWade.jpg';
-import Helena from './assets/htm-people/HelenaBower.jpg';
-import Hugh from './assets/htm-people/HughWells.jpg';
-import Jon from './assets/htm-people/jon.jpg';
-import Justin from './assets/htm-people/JustinChadwell.jpg';
-import Liam from './assets/htm-people/LiamSorta.jpg';
-import Likkan from './assets/htm-people/LikkanChung.jpg';
-import Tom from './assets/htm-people/TomGoodman.jpg';
-import Avery from './assets/htm-people/AveryCunningham.jpg';
-import Jacob from './assets/htm-people/JaconWilson.jpg';
-
-const teamMembers = [
-    {
-        image: Tom,
-        name: 'Tom Goodman',
-        nickname: 'Boss Man',
-        position: 'CEO / Lead Organiser',
-    },
-    {
-        image: Liam,
-        name: 'Liam Sorta',
-        nickname: 'Bigger Boss Man',
-        position: 'CMO / Delight Lead',
-    },
-    {
-        image: Helena,
-        name: 'Helena Bower',
-        nickname: 'The True HTM Overlord',
-        position: 'COO / Publicity Lead',
-    },
-    {
-        image: Charlie,
-        name: 'Charlie de Freitas',
-        nickname: 'Scary Questions Boi',
-        position: 'Logistics Lead',
-    },
-    {
-        image: AlexW,
-        name: 'Alex Wiley',
-        nickname: 'Website Genie',
-        position: 'Creative Lead',
-    },
-    {
-        image: Justin,
-        name: 'Justin Chadwell',
-        nickname: '1337 H4cKerM4N',
-        position: 'Web and CTF Lead',
-    },
-    {
-        image: Fraser,
-        name: 'Fraser Wade',
-        nickname: 'Mesh Router Fanatic',
-        position: 'Networking Lead',
-    },
-    {
-        image: DanielS,
-        name: 'Daniel Spencer',
-        nickname: 'Lord of the Bots',
-        position: 'Web and AV',
-    },
-    {
-        image: Likkan,
-        name: 'Likkan Chung',
-        nickname: 'Count Tweetula',
-        position: 'Publicity Officer',
-    },
-    {
-        image: Jon,
-        name: 'Jon Freer',
-        nickname: 'Stream God',
-        position: 'Production Lead',
-    },
-    {
-        image: Hugh,
-        name: 'Hugh Wells',
-        nickname: 'King Crab',
-        position: 'Logistics',
-    },
-    {
-        image: Avery,
-        name: 'Avery Cunningham',
-        nickname: 'Token Engineer',
-        position: 'Outreach',
-    },
-    {
-        image: Jacob,
-        name: 'Jacob Wilson',
-        nickname: 'Bowtie Zealot',
-        position: 'Publicity Officer',
-    },
-];
-
-const Team = () => (
-    <Layout>
-        <HeroHeader style={{ paddingBottom: '20vw' }}>
-            <h1 className={style.title}>Meet The Team</h1>
-        </HeroHeader>
-
-        <section className={style.team}>
-            <Grid fluid>
-                <Row>
-                    <Col lg={6} lgOffset={3}>
-                        <img src={FireGif} className={style.fire} />
-                        <h2 className={style.teamTitle}>We couldn&apos;t do it without these amazing people</h2>
-                    </Col>
-                </Row>
-                <Row>
-                    { teamMembers.map(({
-                        image, name, nickname, position,
-                    }) => (
-                        <Col lg={3} md={6} sm={12} style={{ marginBottom: '20px' }}>
-                            <TeamCard image={image} name={name} nickname={nickname} position={position} />
+            <section className={style.team}>
+                <Grid fluid>
+                    <Row>
+                        <Col lg={6} lgOffset={3}>
+                            <img src={FireGif} className={style.fire} />
+                            <h2 className={style.teamTitle}>
+                                We couldn&apos;t do it without these amazing
+                                people
+                            </h2>
                         </Col>
-                    ))}
-                    <Row />
-                </Row>
-            </Grid>
-        </section>
-
-    </Layout>
-);
+                    </Row>
+                    <Row>
+                        {teamMembers.map(
+                            ({ image, name, nickname, position }) => (
+                                <Col
+                                    lg={3}
+                                    md={6}
+                                    sm={12}
+                                    style={{ marginBottom: '20px' }}>
+                                    <TeamCard
+                                        image={image.gatsbyImageData}
+                                        name={name}
+                                        nickname={nickname}
+                                        position={position}
+                                    />
+                                </Col>
+                            ),
+                        )}
+                        <Row />
+                    </Row>
+                </Grid>
+            </section>
+        </Layout>
+    );
+};
 
 export default Team;
