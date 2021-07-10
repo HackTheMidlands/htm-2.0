3d7a3cb2628d93379a9d4b3a9180e43d355a7c35
Charles de Freitas <me@chasbob.dev>
2021-07-10 15:07:10 +0100

    Update to Gatsby 3
    - Configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,6 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-  },
-  "extends": "airbnb",
-  "plugins": [
-    "react",
-  ],
-  "globals": {
-    "graphql": false,
-  },
-  "rules": {
-    // Indent with 4 spaces
-    "indent": [2, 4, {"SwitchCase": 1}],
-
-    // Indent JSX with 4 spaces
-    "react/jsx-indent": ["error", 4],
-
-    // Indent props with 4 spaces
-    "react/jsx-indent-props": ["error", 4],
-
-    "react/forbid-prop-types": 0,
-
-    "react/jsx-props-no-spreading": 0,
-
-    "import/prefer-default-export": 0,
-
-    "react/jsx-one-expression-per-line": 0,
-
-    "no-return-assign": 0,
-
-    "max-len": ["error", {"code": 150}],
-
-    "react/prop-types": [2, {"ignore": ["store", "mainState", "globalState", "actions", "children"]}],
-
-    "no-nested-ternary": 0
-
-  },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true,
+    globals: {
+        __PATH_PREFIX__: true,
     },
-  }
-}
+    extends: `react-app`,
+};
