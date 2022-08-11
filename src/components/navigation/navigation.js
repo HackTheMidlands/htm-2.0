f39eaa65b809ba0764d6392e7af4af0d1130b25b
Charles de Freitas <me@chasbob.dev>
2022-08-11 08:57:07 +0100

    Add MLH banner

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -40,18 +40,18 @@ const navigationItems = [
 
 const Navigation = () => (
     <nav role="navigation" className={styles.navigation}>
-        {/* <a */}
-        {/*     className={styles.badge} */}
-        {/*     id="mlh-trust-badge" */}
-        {/*     href="https://mlh.io/seasons/2021/events" */}
-        {/*     target="_blank" */}
-        {/* > */}
-        {/*     <img */}
-        {/*         src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-white.svg" */}
-        {/*         alt="Major League Hacking 2021 Hackathon Season" */}
-        {/*         style={{ width: '100%' }} */}
-        {/*     /> */}
-        {/* </a> */}
+        <a
+            className={styles.badge}
+            id="mlh-trust-badge"
+        href="https://mlh.io/eu?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2023-season&utm_content=white" 
+            target="_blank" rel="noreferrer"
+        >
+            <img
+                src="https://s3.amazonaws.com/logged-assets/trust-badge/2023/mlh-trust-badge-2023-white.svg"
+                alt="Major League Hacking 2023 Hackathon Season"
+                style={{ width: '100%' }}
+            />
+        </a>
         <Grid fluid>
             <Row
                 between="xs"
